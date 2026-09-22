fbb0d328029242edf106db9f0bd18ca43097e5fa
zelibobi4 <zelibobi4@gmail.com>
2026-09-22 12:46:59 +0300

    Add specialty-wide watch target to user model

--- src/models/pydantic_models.py
+++ src/models/pydantic_models.py
@@ -22,6 +22,10 @@ class DbUser(BaseModel):
     limit_days: Optional[int] = None
     time_from_minutes: Optional[int] = None
     time_to_minutes: Optional[int] = None
+    watch_mode: str = "doctor"
+    target_district_id: Optional[str] = None
+    target_lpu_id: Optional[int] = None
+    target_specialty_id: Optional[str] = None
 
 
 class DbDoctorWithUsers(DbDoctor):
